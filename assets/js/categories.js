dce409c2a49376de1d69fed670d03aa99e703304
lumi <118164717+lumijoe@users.noreply.github.com>
2023-09-10 16:04:18 +0900

    0910push

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -66,31 +66,48 @@ observer.observe(fadeUpOne);
 observer.observe(fadeUpTwo);
 
 
-//横スクロール
+//横スクロールはドラッグされているかのマウスのフラグ設定と、x軸座標位置が重要
+//マウス取扱
+/* mousedown=押された瞬間grabbing
+ * mouseup=押されて離れた瞬間nograbbing
+ * mouseleave=要素からマウスが離れた時nohoverの終了
+ * mousemove=マウスが移動した時（hoverしていてもしていなくても）
+*/ 
+//htmlの.slide-containerクラスの要素を取得しslideContainerに代入する
 const slideContainer = document.querySelector('.slide-container');
+//マウスがドラッグ中かを判定するフラグ（初期状態ではドラッグ中ではないのでfalse)
 let isDragging = false;
+//マウスドラッグ開始時のマウス座標、スクロール位置用の変数を準備
 let startX;
 let scrollLeft;
-
+//マウスが押された瞬間イベントを追加
 slideContainer.addEventListener('mousedown', (e) => {
+    //ドラッグ中フラグをtrueにする
     isDragging = true;
+    //（マウスが押された位置x）-（左端xの位置）を取得
     startX = e.pageX - slideContainer.offsetLeft;
+    //左にスクロールした位置を取得（左30=30,右30=-30)
     scrollLeft = slideContainer.scrollLeft;
 });
-
+//マウスが離された瞬間はドラッグはしていないのでfalseフラグを設定
 slideContainer.addEventListener('mouseup', () => {
     isDragging = false;
 });
-
+//マウスが要素から離れたらドラッグはしていないのでfalseフラグを設定
 slideContainer.addEventListener('mouseleave', () => {
     isDragging = false;
 });
-
+//マウスが移動した時、イベントリスナーを追加
 slideContainer.addEventListener('mousemove', (e) => {
+    //ドラッグ中でない場合は次の処理はスキップ（ドラッグしたら次の処理を実行）
     if (!isDragging) return;
+    //イベントのデフォルト動作を無効にする
     e.preventDefault();
+    //（マウスが押された位置x）-（左端xの位置）を取得＝移動した位置を取得
     const x = e.pageX - slideContainer.offsetLeft;
+    //ドラッグ開始位置から移動量を計算、スクロール位置に反映
     const walk = (x - startX) * 1; // スクロール速度調整
+    //マウスの移動方向に合わせて、要素内のコンテンツをスクロールさせる
     slideContainer.scrollLeft = scrollLeft - walk;
 });
 
